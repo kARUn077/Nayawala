94e197186d67816c45e09da8a25f18c84e6be1d0
karun217 <karunpoddar0@gmail.com>
2025-08-08 19:58:54 +0530

    add

--- client/vite-project/src/pages/admin/lecture/LectureTab.jsx
+++ client/vite-project/src/pages/admin/lecture/LectureTab.jsx
@@ -22,7 +22,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "sonner";
 
-const MEDIA_API = "https://teach-dex.vercel.app/api/v1/media";
+const MEDIA_API = "https://udemy-tech.onrender.com/api/v1/media";
 
 const LectureTab = () => {
   const params = useParams();
